a08a0e2e83adf9deab61f116856ffaafb634e515
Jérôme Theytaz <jerome.theytaz@gmail.com>
2023-04-25 23:19:45 -0400

    Edited function, exercice00.py

--- exercice00.py
+++ exercice00.py
@@ -3,16 +3,13 @@ def calendar():
     semaines = ["lundi, mardi, mercredi, jeudi, vendredi, samedi, dimanche"]
     index = 0
     for jour in semaines:
+        if index >= 1 or index <= 4:
+            print(jour[index], "Au travail")
+        elif index == 5:
+            print(jour[index], "Chouette c'est vendredi")
+        elif index == 6:
+            print(jour[index], "Repos ce week-end")
         index += 1
-    if index >= 1 or index <= 4:
-        print(jour, "Au travail")
-    elif index == 5:
-        print(jour, "Chouette c'est vendredi")
-    else:
-        print(jour, "Repos ce week-end")
 
 # Resolution
-result = calendar()
-
-# Function to print out result
-print(result)
+calendar()
