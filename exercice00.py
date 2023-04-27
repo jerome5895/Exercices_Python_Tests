dc518fa8c03ed668c6b0f5f5c6c97d309b509672
Jérôme Theytaz <jerome.theytaz@gmail.com>
2023-04-26 20:52:11 -0400

    Edited function calendar, exercice00.py

--- exercice00.py
+++ exercice00.py
@@ -1,15 +1,11 @@
 # Function to print out the seven days of the week, with the corresponding messages
 def calendar():
-    semaines = ["lundi, mardi, mercredi, jeudi, vendredi, samedi, dimanche"]
+    semaines = ["lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi", "dimanche"]
     index = 0
     for jour in semaines:
-        if index >= 1 or index <= 4:
-            print(jour[index], "Au travail")
-        elif index == 5:
-            print(jour[index], "Chouette c'est vendredi")
-        elif index == 6:
-            print(jour[index], "Repos ce week-end")
         index += 1
+        if index <= 4:
+            print(f"{jour}: Au travail")
 
 # Resolution
 calendar()
