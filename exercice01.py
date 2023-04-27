be5240bfdd7d606f02b6692522cc3d36ee768aa2
Jérôme Theytaz <jerome.theytaz@gmail.com>
2023-04-26 21:14:28 -0400

    Edited function seq_comp, exercice01.py

--- exercice01.py
+++ exercice01.py
@@ -1,6 +1,15 @@
 def seq_comp():
     sequence = ["A", "C", "G", "T", "T", "A", "G", "C", "T", "A", "A", "C", "G"]
     for seq in sequence:
-        print(seq)
+        if seq == "A":
+            print("T")
+        elif seq == "T":
+            print("A")
+        elif seq == "C":
+            print("G")
+        elif seq == "G":
+            print("C")
+        else:
+            print(seq)
 
 seq_comp()
