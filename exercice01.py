828f7acf4712e4df7be045d7a7a2412342247b7d
Jérôme Theytaz <jerome.theytaz@gmail.com>
2023-04-26 21:16:19 -0400

    Added comments, exercice01.py

--- exercice01.py
+++ exercice01.py
@@ -1,3 +1,4 @@
+# Function to change sequence to complementary sequence
 def seq_comp():
     sequence = ["A", "C", "G", "T", "T", "A", "G", "C", "T", "A", "A", "C", "G"]
     for seq in sequence:
@@ -12,4 +13,5 @@ def seq_comp():
         else:
             print(seq)
 
+# Print out result
 seq_comp()
