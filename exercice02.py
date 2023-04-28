83f02a01f3afab825c97b8383bccc213697a916b
Jérôme Theytaz <jerome.theytaz@gmail.com>
2023-04-28 00:16:36 -0400

    Modified sequence, edited comments, exercice02.py

--- exercice02.py
+++ exercice02.py
@@ -1,14 +1,14 @@
 import sys
 
-# Function to determine the smaller 
+# Function to determine the smaller number of a list 
 def smaller(numbers):
     for i in numbers:
         index = i
         for j in numbers:
             if int(numbers[j]) < int(numbers[index]):
                 index = j
-                numbers{i}, numbers{index} == numbers{index}, numbers{i}
-    return 
+                numbers[i], numbers[index] == numbers[index], numbers[i]
+    return numbers[index]
 
 # Globales variables
 numbers = sys.argv[1]
