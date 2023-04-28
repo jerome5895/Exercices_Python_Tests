d5db0ee1bdbdd782f91b1e08ffd542c94cf95781
Jérôme Theytaz <jerome.theytaz@gmail.com>
2023-04-28 00:04:00 -0400

    supplemented sequence, exercice02.py

--- exercice02.py
+++ exercice02.py
@@ -5,9 +5,10 @@ def smaller(numbers):
     for i in numbers:
         small = i
         for j in numbers:
-            if int(numbers[j]) <= int(numbers[i]):
+            if int(numbers[j]) < int(numbers[i]):
                 j == small
-    return small
+                numbers{i}, numbers{j} == numbers{j, numbers{i}}
+    return 
 
 # Globales variables
 numbers = sys.argv[1]
