2fae46d7f18c774493d9763f368866e38c2d98ba
Jérôme Theytaz <jerome.theytaz@gmail.com>
2023-04-27 23:53:18 -0400

    Added comments, globales variable and function parameter, exercice02.py

--- exercice02.py
+++ exercice02.py
@@ -1,10 +1,16 @@
+import sys
 
 # Function to determine the smaller 
-def smaller():
-    numbers = [8, 4, 6, 1, 5]
+def smaller(numbers):
     for i in numbers:
-    
-    return
+        small = i
+        for j in numbers:
+            if numbers[j] <= numbers[i]:
+                j == small
+    return small
+
+# Globales variables
+numbers = sys.argv[1]
 
 # Print out result
-smaller()
+smaller(numbers)
