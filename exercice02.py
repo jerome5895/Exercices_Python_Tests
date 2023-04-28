19438ef0a4824e154a69e789212f5623e126c7cf
Jérôme Theytaz <jerome.theytaz@gmail.com>
2023-04-28 00:22:57 -0400

    Modified sequence and globale variable, exercice02.py

--- exercice02.py
+++ exercice02.py
@@ -5,15 +5,15 @@ def smaller(numbers):
     for i in numbers:
         index = i
         for j in numbers:
-            if int(numbers[j]) < int(numbers[index]):
+            if numbers[j] < numbers[index]:
                 index = j
                 numbers[i], numbers[index] == numbers[index], numbers[i]
     return numbers[index]
 
 # Globales variables
-numbers = sys.argv[1]
+numbers = [int(numbers) for numbers in sys.argv[1]]
 
-# Print out result
+# Resolution
 result = smaller(numbers)
 
 # Print out result
