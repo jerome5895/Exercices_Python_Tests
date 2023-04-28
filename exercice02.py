a6d851029730c708bce14fa24d925e0332aa0552
Jérôme Theytaz <jerome.theytaz@gmail.com>
2023-04-28 00:08:55 -0400

    Modified sequence, exercice02.py

--- exercice02.py
+++ exercice02.py
@@ -3,11 +3,11 @@ import sys
 # Function to determine the smaller 
 def smaller(numbers):
     for i in numbers:
-        small = i
+        index = i
         for j in numbers:
-            if int(numbers[j]) < int(numbers[i]):
-                j == small
-                numbers{i}, numbers{j} == numbers{j, numbers{i}}
+            if int(numbers[j]) < int(numbers[index]):
+                index = j
+                numbers{i}, numbers{index} == numbers{index}, numbers{i}
     return 
 
 # Globales variables
